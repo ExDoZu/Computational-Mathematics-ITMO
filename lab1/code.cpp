8e7fddc7a496e2091466e3b8f5cbc698538f3a08
ExDo <don't.write.me>
2023-02-17 00:44:50 +0300

    fixed input and small changes

--- lab1/code.cpp
+++ lab1/code.cpp
@@ -1,16 +1,96 @@
+#include <algorithm>
 #include <fstream>
+#include <iomanip>
 #include <iostream>
 #include <set>
 #include <vector>
 
+namespace utils {
 void print_matrix(std::vector<std::vector<double>> const &a) {
-    for (size_t i = 0; i < a.size(); i++) {
-        for (size_t j = 0; j < a[i].size(); j++) {
-            std::cout << a[i][j] << " ";
+    for (auto const &row : a) {
+        for (auto const &element : row) {
+            std::cout << std::setw(9) << std::setfill(' ') << element << " ";
         }
         std::cout << std::endl;
     }
 }
+
+void print_entered(std::vector<std::vector<double>> const &a, std::vector<double> const &b, double const eps) {
+    std::cout << "Погрешность: " << eps << std::endl;
+    std::cout << "Введено СЛАУ:" << std::endl;
+    for (size_t i = 0; i < a.size(); i++) {
+        for (size_t j = 0; j < a[i].size(); j++) {
+            std::cout << a[i][j] << "·x" << j + 1;
+            if (j != a.size() - 1 && a[i][j + 1] > 0) {
+                std::cout << " + ";
+            }
+        }
+        std::cout << " = " << b[i] << std::endl;
+    }
+}
+
+void print_xk(std::vector<double> const &xk) {
+    std::cout << "Xk:" << std::endl
+              << "(";
+    for (size_t i = 0; i < xk.size() - 1; i++) {
+        std::cout << xk[i] << "; ";
+    }
+    std::cout << xk[xk.size() - 1] << ")";
+    std::cout << std::endl;
+}
+
+void print_diffs(std::vector<double> const &diffs) {
+    std::cout << "|x(k)-x(k-1)|:" << std::endl
+              << "(";
+    for (size_t i = 0; i < diffs.size() - 1; i++) {
+        std::cout << diffs[i] << "; ";
+    }
+    std::cout << diffs[diffs.size() - 1] << ")";
+    std::cout << std::endl;
+}
+
+void print_answer(std::vector<double> const &xk) {
+    std::cout << "Ответ:" << std::endl;
+    for (size_t i = 0; i < xk.size(); i++) {
+        std::cout << "x" << i + 1 << " = " << xk[i] << std::endl;
+    }
+}
+
+template <class T>
+bool read_on_endl(std::istream &fin, T &var) {
+    T x;
+    if (fin >> x) {
+        int const c = fin.get();
+        if (c == '\n' || c == '\r' || c == EOF) {
+            var = x;
+            return true;
+        }
+    }
+    return false;
+}
+
+bool read_on_line(std::istream &fin, double &var) {
+    double x;
+    if (fin >> x) {
+        int const c = fin.get();
+        if (c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == EOF) {
+            var = x;
+            return true;
+        }
+    }
+    return false;
+}
+
+void set_precision_by_eps(double const eps) {
+    int32_t precision = 0;
+    double eps_copy = eps;
+    while (eps_copy < 1) {
+        eps_copy *= 10;
+        precision++;
+    }
+    std::cout << std::setprecision(std::max(std::min(precision + 1, 6), 2));
+}
+}  // namespace utils
 
 bool check_current_diagonal_dominance(std::vector<std::vector<double>> const &a) {
     bool flag = false;
@@ -29,6 +109,7 @@ bool check_current_diagonal_dominance(std::vector<std::vector<double>> const &a)
     }
     return flag;
 }
+
 bool check_diagonal_dominance(std::vector<std::vector<double>> &a, std::vector<double> &b) {
     if (check_current_diagonal_dominance(a)) {
         return true;
@@ -102,48 +183,44 @@ std::vector<double> calculate_new_xk(std::vector<std::vector<double>> const &c, 
     }
     return new_xk;
 }
-void print_xk(std::vector<double> const &xk) {
-    std::cout << "Xk = (";
-    for (size_t i = 0; i < xk.size() - 1; i++) {
-        std::cout << xk[i] << "; ";
-    }
-    std::cout << xk[xk.size() - 1] << ")";
-    std::cout << std::endl;
-}
 
 std::vector<double> calculate_answer(std::vector<std::vector<double>> const &a, std::vector<double> const &b, double const eps, bool const print_info = true) {
-    std::vector<std::vector<double>> c = calculate_c(a);
-    std::vector<double> d = calculate_d(a, b);
+    std::vector<std::vector<double>> const c = calculate_c(a);
+    std::vector<double> const d = calculate_d(a, b);
     std::vector<double> xk = d;
-    std::vector<double> new_xk;
+    std::cout << std::fixed;
+    utils::set_precision_by_eps(eps);
     if (print_info) {
         std::cout << "Матрица a: " << std::endl;
-        print_matrix(a);
+        utils::print_matrix(a);
+        std::cout << std::endl;
         std::cout << "Вектор b: " << std::endl;
         for (auto const &i : b) {
             std::cout << i << " ";
         }
-        std::cout << std::endl;
+        std::cout << std::endl
+                  << std::endl;
         std::cout << "Матрица c: " << std::endl;
-        print_matrix(c);
+        utils::print_matrix(c);
+        std::cout << std::endl;
         std::cout << "Вектор d: " << std::endl;
         for (auto const &i : d) {
             std::cout << i << " ";
         }
-        std::cout << std::endl;
-        print_xk(xk);
+        std::cout << std::endl
+                  << std::endl;
+        utils::print_xk(xk);
     }
+    std::vector<double> new_xk;
+    std::vector<double> diffs(xk.size());
     bool flag = true;
     size_t iter = 1;
     while (flag) {
-        double max_diff = 0;
         new_xk = calculate_new_xk(c, d, xk);
         flag = false;
         for (size_t i = 0; i < xk.size(); i++) {
             double const diff = std::abs(new_xk[i] - xk[i]);
-            if (diff > max_diff) {
-                max_diff = diff;
-            }
+            diffs[i] = diff;
             if (diff >= eps) {
                 flag = true;
             }
@@ -151,8 +228,8 @@ std::vector<double> calculate_answer(std::vector<std::vector<double>> const &a, 
         xk = new_xk;
         if (print_info) {
             std::cout << "----- Итерация №" << iter << " -----" << std::endl;
-            print_xk(xk);
-            std::cout << "Максимальная разность: " << max_diff << std::endl;
+            utils::print_xk(xk);
+            utils::print_diffs(diffs);
             iter++;
         }
     }
@@ -160,49 +237,43 @@ std::vector<double> calculate_answer(std::vector<std::vector<double>> const &a, 
 }
 
 bool read_from_file(std::ifstream &file, std::vector<std::vector<double>> &a, std::vector<double> &b, double &eps) {
-    if (file >> eps) {
-        if (file.get() != '\n') {
-            std::cerr << "Некорректный ввод точности." << std::endl;
-            return false;
-        } else if (eps <= 0) {
+    if (utils::read_on_endl(file, eps)) {
+        if (eps <= 0) {
             std::cerr << "Точность должна быть положительным числом." << std::endl;
             return false;
         }
     } else {
-        std::cerr << "Некорректный ввод точности." << std::endl;
+        std::cerr << "Некорректо указана точность." << std::endl;
         return false;
     }
 
     int64_t n;
-    if (file >> n) {
-        if (file.get() != '\n') {
-            std::cerr << "Некорректный ввод размерности." << std::endl;
-            return false;
-        }
+    if (utils::read_on_endl(file, n)) {
         if (n <= 0) {
             std::cerr << "Размерность матрицы должна быть положительным числом." << std::endl;
             return false;
         }
     } else {
-        std::cerr << "Некорректный ввод размерности." << std::endl;
+        std::cerr << "Некорректно указана размерность." << std::endl;
         return false;
     }
     a.resize(n, std::vector<double>(n));
     b.resize(n);
     for (size_t i = 0; i < n; i++) {
         for (size_t j = 0; j < n; j++) {
-            if (!(file >> a[i][j])) {
+            if (!utils::read_on_line(file, a[i][j])) {
                 std::cerr << "Некорректная матрица." << std::endl;
                 return false;
             }
         }
-        if (!(file >> b[i])) {
+        if (!utils::read_on_line(file, b[i])) {
             std::cerr << "Некорректный коэффициент b." << std::endl;
             return false;
         };
     }
     return true;
 }
+
 void read_from_terminal(std::vector<std::vector<double>> &a, std::vector<double> &b, double &eps) {
     auto clear_and_ignore_and_print_error = [] {
         std::cin.clear();
@@ -215,25 +286,22 @@ void read_from_terminal(std::vector<std::vector<double>> &a, std::vector<double>
 
     while (true) {
         std::cout << "Введите точность вычислений: ";
-        if (std::cin >> eps) {
-            if (getchar() != '\n') {
-                clear_and_ignore_and_print_error();
-            } else if (eps > 0) {
+        if (utils::read_on_endl(std::cin, eps)) {
+            if (eps > 0) {
                 break;
             } else {
                 std::cerr << "Точность должна быть положительным числом." << std::endl;
             }
+        } else {
+            clear_and_ignore_and_print_error();
         }
     }
 
     int64_t n;
-
     while (true) {
         std::cout << "Введите размерность матрицы: ";
-        if (std::cin >> n) {
-            if (getchar() != '\n') {
-                clear_and_ignore_and_print_error();
-            } else if (n > 0) {
+        if (utils::read_on_endl(std::cin, n)) {
+            if (n > 0) {
                 break;
             } else {
                 std::cerr << "Размерность матрицы должна быть положительным числом." << std::endl;
@@ -256,12 +324,12 @@ void read_from_terminal(std::vector<std::vector<double>> &a, std::vector<double>
 
         for (size_t i = 0; i < n; i++) {
             for (size_t j = 0; j < n; j++) {
-                if (!(std::cin >> a[i][j])) {
+                if (!utils::read_on_line(std::cin, a[i][j])) {
                     input_error = true;
                     break;
                 }
             }
-            if (input_error || !(std::cin >> b[i])) {
+            if (input_error || !utils::read_on_line(std::cin, b[i])) {
                 input_error = true;
                 break;
             }
@@ -269,27 +337,6 @@ void read_from_terminal(std::vector<std::vector<double>> &a, std::vector<double>
         if (input_error) {
             clear_and_ignore_and_print_error();
         }
-    }
-}
-
-void print_entered(std::vector<std::vector<double>> const &a, std::vector<double> const &b, double const eps) {
-    std::cout << "Погрешность: " << eps << std::endl;
-    std::cout << "Введено СЛАУ:" << std::endl;
-    for (size_t i = 0; i < a.size(); i++) {
-        for (size_t j = 0; j < a[i].size(); j++) {
-            std::cout << a[i][j] << "·x" << j + 1;
-            if (j != a.size() - 1 && a[i][j + 1] > 0) {
-                std::cout << " + ";
-            }
-        }
-        std::cout << " = " << b[i] << std::endl;
-    }
-}
-
-void print_answer(std::vector<double> const &xk) {
-    std::cout << "Ответ:" << std::endl;
-    for (size_t i = 0; i < xk.size(); i++) {
-        std::cout << "x" << i + 1 << " = " << xk[i] << std::endl;
     }
 }
 
@@ -302,7 +349,7 @@ void solve_sole(int const argc, char const *argv[]) {
         case 1:
             std::cout << "Файл не был указан. Чтение из терминала." << std::endl;
             read_from_terminal(a, b, eps);
-            print_entered(a, b, eps);
+            utils::print_entered(a, b, eps);
             break;
         case 2: {
             std::cout << "Чтение из файла " << argv[1] << std::endl;
@@ -310,7 +357,7 @@ void solve_sole(int const argc, char const *argv[]) {
             if (!read_from_file(file, a, b, eps))
                 return;
             file.close();
-            print_entered(a, b, eps);
+            utils::print_entered(a, b, eps);
             break;
         }
         default:
@@ -321,7 +368,7 @@ void solve_sole(int const argc, char const *argv[]) {
         std::cout << "Матрица не удовлетворяет условию диагонального преобладания" << std::endl;
         return;
     }
-    print_answer(calculate_answer(a, b, eps));
+    utils::print_answer(calculate_answer(a, b, eps));
 }
 
 int main(int const argc, char const *argv[]) {
